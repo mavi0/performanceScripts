4c07be7fdd02dd1a5faea51e9d4f7a6ee2b742f8
Eleanor Davies <ellie@mavieson.co.uk>
2019-01-09 11:05:20 +0000

    cron

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Scripts to monitor network performance
 sudo apt install -y python3 iperf3 screen python-pip python3-pip speedtest-cli golang-go autossh
 pip3 install iperf3 pingparsing
 ```
-Maybe necessary to add export PATH=$PATH:~/.local/bin to ~/.bashrc or ~/.bash_profile
+May be necessary to add export PATH=$PATH:~/.local/bin to ~/.bashrc or ~/.bash_profile
+
+Load crontab with ```crontab client/cronjobs```
--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ from subprocess import check_output
 from datetime import datetime
 from time import sleep
 
-## TO DO: also save json files to unique file, for logs. ALSO retry iperf when can't connect
+# TO DO: also save json files to unique file, for logs. ALSO retry iperf when can't connect
 config = configparser.ConfigParser()
 time = datetime.now()
 
@@ -21,7 +21,8 @@ num_streams = 4
 base_port = int(config['DEFAULT']['Port'])
 server_hostname = config['DEFAULT']['Hostname']
 
-def iperf( port ):
+
+def iperf(port):
     client = iperf3.Client()
     print("Performing iperf test.....")
     client.duration = duration
@@ -35,7 +36,7 @@ def iperf( port ):
     result = client.run()
     if result.error:
         print(result.error)
-        #if the server is busy try a new port - there are 4 daemons running. Iterate though these until we get a success
+        # if the server is busy try a new port - there are 4 daemons running. Iterate though these until we get a success
         port += 1
         if port > base_port + 4:
             port = base_port
@@ -51,16 +52,18 @@ def iperf( port ):
         with open('iperfLogs/%s.json' % time, 'w') as iperf_log:
             json.dump(result.json, iperf_log)
 
+
 iperf(base_port)
 
 print("Complete!\n\nPerforming latency test....")
 ping_json = json.loads(check_output(["pingparsing", server_hostname]))
 ping_json[server_hostname]['jitter'] = ping_json[server_hostname]["rtt_max"] - ping_json[server_hostname]["rtt_min"]
-json_hostname = server_hostname.replace(".", "_")                        #fix for zabbix. cant escape periods
+# fix for zabbix. cant escape periods
+json_hostname = server_hostname.replace(".", "_")
 ping_json_hostname = {}
 ping_json_hostname[json_hostname] = ping_json.pop(server_hostname)
 
-with open('ping.json' , 'w') as ping_file:
+with open('ping.json', 'w') as ping_file:
     json.dump(ping_json_hostname, ping_file)
 
 with open('pingLogs/%s.json' % time, 'w') as ping_log:
@@ -72,8 +75,8 @@ try:
     speedtest_json = json.loads(check_output(["speedtest-cli", "--json"]))
 except subprocess.CalledProcessError as e:
     speedtest_json =
-with open('speedtest.json' , 'w') as speedtest_file:
-        json.dump(speedtest_json, speedtest_file)
+with open('speedtest.json', 'w') as speedtest_file:
+    json.dump(speedtest_json, speedtest_file)
 
 with open('speedtestLogs/%s.json' % time, 'w') as speedtest_log:
     json.dump(speedtest_json, speedtest_log)
